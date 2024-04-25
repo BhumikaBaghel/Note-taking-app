dec0d94d12bb60c850138bb6111707129dac0e97
Bhumika07092001 <109783089+Bhumika07092001@users.noreply.github.com>
2024-04-26 01:11:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 import Routes from "./Routes";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+    <ToastContainer/>
       <Router>
         <Routes />
       </Router>
